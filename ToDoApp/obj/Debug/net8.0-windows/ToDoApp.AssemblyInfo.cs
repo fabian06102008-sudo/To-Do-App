71088d4c2a1cfdd6cf712b6af26e15b184f894ba
rodriguez-jp <juanrdrz16@gmail.com>
2025-07-30 21:38:10 -0500

    test

--- ToDoApp/obj/Debug/net8.0-windows/ToDoApp.AssemblyInfo.cs
+++ ToDoApp/obj/Debug/net8.0-windows/ToDoApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a57654c367e9aadb7ea3e2ab24b7512a87745c3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
