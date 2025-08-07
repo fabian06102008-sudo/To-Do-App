04542d86e746743beae1cfd810344b9077205d2d
Rodriguez-Jp <juanrdrz16@gmail.com>
2025-08-07 17:22:55 -0500

    started working on saving all task in JSON files

--- ToDoApp/ViewModel/MainViewModel.cs
+++ ToDoApp/ViewModel/MainViewModel.cs
@@ -2,23 +2,30 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ToDoApp.Model;
+using System.Text.Json;
+using System.Windows;
+
 
 namespace ToDoApp.ViewModel
 {
     public class MainViewModel : INotifyPropertyChanged
     {
-
+        
         public MainViewModel()
         {
-            _allTodos = new ObservableCollection<Todo>()
-            {
-                new Todo() {Description = "Get out with friends", IsDone = false}
-            };
+            _allTodos = new ObservableCollection<Todo>();
+            Application.Current.Exit += OnApplicationExit;
 
+            using (StreamReader sr = new StreamReader("C:\\Users\\JuanR\\Desktop\\C#\\Projects\\ToDoApp\\ToDoApp\\tasksList.json"))
+            {
+                string json = sr.ReadToEnd();
+                _allTodos = JsonSerializer.Deserialize<ObservableCollection<Todo>>(json);
+            }
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
@@ -36,8 +43,12 @@ namespace ToDoApp.ViewModel
                 _allTodos = value;
                 OnPropertyChanged(nameof(AllTodos));
             }
+        }
 
+        private void OnApplicationExit(object sender, ExitEventArgs e)
+        {
 
         }
+
     }
 }
